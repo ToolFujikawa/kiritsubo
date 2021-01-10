42b481d43c6344580363684501720d3ffa24ead4
藤川善行 <yoshiyuki.fujikawa@gmail.com>
2021-01-11 05:20:35 +0900

    プロジェクト ファイルを追加します。

---
+++ Target19_Relationship/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Target19_Relationship.MvcApplication" Language="C#" %>
---
+++ Target19_Relationship/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Target19_Relationship.Startup))]
+namespace Target19_Relationship
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
